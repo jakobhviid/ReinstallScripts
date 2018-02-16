1d6e1b02bc3bc5aaf31395ea40126e5c5785a026
Jakob Hviid <jakob@hviidnet.com>
2018-02-16 18:06:37 +0100

    removed installer from software.cmd

--- Win/Software.cmd
+++ Win/Software.cmd
@@ -1,5 +1,3 @@
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
 echo Installing Standard tools like chrome, adobe reader, java, 7zip etc.
 choco install googlechrome -y
 choco install lastpass -y

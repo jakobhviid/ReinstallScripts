4f29fef8ec736a4d202383839b9e8aeb75583695
Jakob Hviid <jakob@hviidnet.com>
2019-04-17 08:19:15 +0200

    a multitde of fixes

--- Win/software.cmd
+++ Win/software.cmd
@@ -1,84 +1,81 @@
-echo Installing Standard tools like chrome, adobe reader, java, 7zip etc.
-choco install googlechrome -y
-choco install lastpass -y
-choco install adobereader -y
-choco install jre8 -y
-choco install 7zip.install -y
-choco install vlc -y
-choco install paint.net -y
-choco install dropbox -y
-choco install chocolateygui -y
-choco install teamviewer -y
-choco install sharex -y
+echo off
+cd c:\
 
-choco install itunes -y
-choco install spotify -y
+echo Installing chocolatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+echo Enabling Global Confirmation
+choco feature enable -n=allowGlobalConfirmation
+
+echo Installing Standard tools like chrome, adobe reader, java, 7zip etc.
+choco install 1password
+choco install googlechrome
+choco install adobereader
+choco install jre8
+choco install 7zip.install
+choco install vlc
+choco install paint.net
+choco install teamviewer
+choco install spotify
 
 echo Installing hardware related helper tools
-choco install logitechgaming -y
-choco install logitech-options -y
-rem choco install setpoint -y
+choco install logitechgaming
+choco install logitech-options
+rem choco install setpoint
 
 echo Installing Game Related Packages
-choco install discord -y
-choco install steam -y
-choco install origin -y
-choco install uplay -y
-choco install geforce-experience -y
-rem choco install geforce-game-ready-driver -y
+choco install discord
+choco install steam
+choco install origin
+choco install uplay
+choco install epicgameslauncher
+choco install geforce-experience
 
 echo Installing commandline tools
-choco install curl -y
-choco install nmap -y
-choco install sudo -y
+choco install curl
+choco install nmap
+choco install sudo
 
 echo Installing Developer Tools
-choco install golang -y
-choco install jdk8 -y
-choco install filezilla -y
-choco install putty.install -y
-choco install git.install -y
-choco install gitkraken -y
-choco install visualstudiocode -y
-choco install sqlitebrowser -y
-choco install typescript -y
-choco install wudt -y
+choco install vscode
+choco install git.install
+choco install gitkraken
+choco install filezilla
+choco install golang
+choco install jdk8
+choco install putty.install
+choco install visualstudiocode
+choco install sqlitebrowser
+choco install typescript
+choco install wudt
 
 REM Needed for LaTeX
 echo Installing tools for LaTeX
-choco install miktex -y
-choco install synctex -y
-choco install activeperl -y
+REM choco install miktex
+choco install synctex
+REM choco install activeperl
+choco install strawberryperl
 refreshenv
 cpan -f -i Unicode::GCString
 cpan -f -i YAML::Tiny
 cpan -f -i Log::Dispatch::File
 cpan -f -i Log::Log4perl
-choco install python -y
+choco install python
 refreshenv
 python -m pip install --upgrade pip
 pip install distribute
 pip install pygments
+choco install anaconda3
+choco install nodejs.install
 
-choco install nodejs.install -y
-REM choco install yarn -y
-REM call npm install -g yo
-REM call npm install -g bower
-REM call npm install -g grunt-cli
-REM REM call npm install -g yarn  - installed using choco instead
-REM call npm install -g webpack
+REM Generating SSH keys for git etc.
+cd %userprofile%/.ssh
+ssh-keygen -t rsa -b 4096 -C "jakob@hviidnet.com"
+
+REM Installing Windows System Features
+powershell Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 
-echo Installing Large Developer Tools Packages
-REM choco install visualstudio2017community -y
-choco install visualstudio2017enterprise -y
-choco install visualstudio2017-workload-netweb -y
-choco install visualstudio2017-workload-manageddesktop -y
-choco install visualstudio2017-workload-data -y
-choco install visualstudio2017-workload-netcoretools -y
-choco install resharper -y
-choco install sql-server-express -y
-choco install sql-server-management-studio -y
+echo remember to install tex live - downloading to the downloads folder now:
+curl http://mirrors.dotsrc.org/ctan/systems/texlive/tlnet/install-tl-windows.exe --output C:\Users\%USERNAME%\Downloads\install-tl-windows.exe
 
-echo Installing Office
-REM choco install office365homepremium -y
+echo remember to install Office 365 too.

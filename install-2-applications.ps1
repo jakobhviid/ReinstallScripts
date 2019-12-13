5d96ab0bbb510f7b47e357b141b8cd9150526620
Jakob Hviid <jakobhviid1982@gmail.com>
2019-12-13 19:47:05 +0100

    fixed dependency issues in scoop

--- install-2-applications.ps1
+++ install-2-applications.ps1
@@ -1,7 +1,8 @@
+scoop install git
+scoop install aria2 sudo
 scoop bucket add extras
 scoop bucket add java
-scoop install aria2 sudo
-scoop install curl grep nano vim make say tar sudo micro coreutils git tidy 7zip oraclejre8 vlc 7zip paint.net handbrake teamviewer discord vscode gitkraken filezilla sqlitebrowser putty dotnet-sdk anaconda3 nodejs python go
+scoop install curl grep nano vim make say tar sudo micro coreutils git tidy 7zip oraclejre8 vlc 7zip paint.net handbrake teamviewer discord vscode gitkraken filezilla sqlitebrowser putty dotnet-sdk anaconda3 nodejs python go caprine
 
 regedit /s ./supportfiles/FixGitKraken.reg
 regedit /s ./supportfiles/DisableNetworkDriveWarning.reg

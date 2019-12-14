006696100f43b489ae9fd0b55a66e0eac39785a2
Jakob Hviid <jakobhviid1982@gmail.com>
2019-12-14 20:35:56 +0100

    added teams and handbrake

--- install-2-applications.ps1
+++ install-2-applications.ps1
@@ -2,7 +2,7 @@ scoop install git
 scoop install aria2 sudo
 scoop bucket add extras
 scoop bucket add java
-scoop install curl grep nano vim make say tar sudo micro coreutils git tidy 7zip oraclejre8 vlc 7zip paint.net handbrake teamviewer discord vscode gitkraken filezilla sqlitebrowser putty dotnet-sdk anaconda3 nodejs python go caprine
+scoop install curl grep nano vim make say tar sudo micro coreutils git tidy oraclejre8 vlc 7zip paint.net handbrake teamviewer discord vscode gitkraken filezilla sqlitebrowser putty dotnet-sdk anaconda3 nodejs python go caprine
 
 regedit /s ./supportfiles/FixGitKraken.reg
 regedit /s ./supportfiles/DisableNetworkDriveWarning.reg
@@ -14,3 +14,6 @@ choco install activeperl
 cd ~\Downloads\
 wget https://dl.appget.net/appget/appget.setup.exe -o appget.exe 
 ./appget.exe /sp- /silent /norestart
+
+appget install teams
+appget install handbrake

75cf4b4da6c28a3d9c7d76f90d2ea983b4a89dcf
Jakob Hviid <jakobhviid1982@gmail.com>
2020-01-07 15:21:25 +0100

    added ssh-copy-id

--- install-2-applications.ps1
+++ install-2-applications.ps1
@@ -5,7 +5,7 @@ git config --global user.email "jakobhviid1982@gmail.com"
 scoop install aria2 sudo
 scoop bucket add extras
 scoop bucket add java
-scoop install curl grep nano vim make say tar sudo micro coreutils git tidy oraclejre8 vlc 7zip paint.net handbrake teamviewer discord vscode gitkraken filezilla sqlitebrowser putty dotnet-sdk anaconda3 nodejs python go caprine sharpkeys nssm draw.io
+scoop install curl grep nano vim make say tar sudo micro coreutils git tidy oraclejre8 vlc 7zip paint.net handbrake teamviewer discord vscode gitkraken filezilla sqlitebrowser putty dotnet-sdk anaconda3 nodejs python go caprine sharpkeys nssm draw.io ssh-copy-id
 
 regedit /s ./supportfiles/FixGitKraken.reg
 regedit /s ./supportfiles/DisableNetworkDriveWarning.reg

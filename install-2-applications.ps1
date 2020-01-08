87db78fff511ead78f769f47b05d4eeb6e5511f7
Jakob Hviid <jakobhviid1982@gmail.com>
2020-01-08 10:38:45 +0100

    moved several apps to appget and scoop instead of choco

--- install-2-applications.ps1
+++ install-2-applications.ps1
@@ -5,14 +5,13 @@ git config --global user.email "jakobhviid1982@gmail.com"
 scoop install aria2 sudo
 scoop bucket add extras
 scoop bucket add java
-scoop install curl grep nano vim make say tar sudo micro coreutils git tidy oraclejre8 vlc 7zip paint.net handbrake teamviewer discord vscode gitkraken filezilla sqlitebrowser putty dotnet-sdk anaconda3 nodejs python go caprine sharpkeys nssm draw.io ssh-copy-id
+scoop install curl grep nano vim make say tar sudo micro coreutils git tidy oraclejre8 vlc 7zip paint.net vscode gitkraken filezilla sqlitebrowser putty anaconda3 nodejs python go caprine sharpkeys nssm draw.io ssh-copy-id perl
 
 regedit /s ./supportfiles/FixGitKraken.reg
 regedit /s ./supportfiles/DisableNetworkDriveWarning.reg
 
 choco feature enable -n=allowGlobalConfirmation
 choco install synctex
-choco install activeperl
 
 Set-Location ~\Downloads\
 Invoke-WebRequest https://dl.appget.net/appget/appget.setup.exe -o appget.exe 
@@ -20,3 +19,4 @@ Invoke-WebRequest https://dl.appget.net/appget/appget.setup.exe -o appget.exe
 
 appget install teams
 appget install handbrake
+appget install teamviewer

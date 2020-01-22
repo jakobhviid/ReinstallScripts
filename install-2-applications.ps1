f8b1d370840a02459514aa7224bf0c13ff7acf39
Jakob Hviid <jakobhviid1982@gmail.com>
2020-01-22 12:08:01 +0100

    added cascadia code install

--- install-2-applications.ps1
+++ install-2-applications.ps1
@@ -42,3 +42,31 @@ cpan -f -i Log::Log4perl
 
 # extra apps for scientific work
 scoop install protege
+
+# Installing cascadia code fonts (update them from https://github.com/microsoft/cascadia-code/releases)
+
+$SourceDir = ".\supportfiles\fonts"
+$Source = ".\supportfiles\fonts\*"
+$Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+$TempFolder = "C:\Windows\Temp\Fonts"
+
+# Create the source directory if it doesn't already exist
+New-Item -ItemType Directory -Force -Path $SourceDir
+
+New-Item $TempFolder -Type Directory -Force | Out-Null
+
+Get-ChildItem -Path $Source -Include '*.ttf', '*.ttc', '*.otf' -Recurse | ForEach {
+    If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {
+
+        $Font = "$TempFolder\$($_.Name)"
+        
+        # Copy font to local temporary folder
+        Copy-Item $($_.FullName) -Destination $TempFolder
+        
+        # Install font
+        $Destination.CopyHere($Font, 0x10)
+
+        # Delete temporary copy of font
+        Remove-Item $Font -Force
+    }
+}

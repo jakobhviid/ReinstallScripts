f37b0fc66817e14770ad4526337c8bf6f678c221
Jakob Hviid <jakobhviid1982@gmail.com>
2020-04-01 13:45:56 +0200

    added letsencrypt tools for iis

--- install-2-applications.ps1
+++ install-2-applications.ps1
@@ -21,7 +21,7 @@ scoop bucket add extras
 scoop bucket add java
 scoop bucket add nerd-fonts
 scoop bucket add specialized https://github.com/jakobhviid/scoop-specialized
-scoop install busybox curl grep nano vim make say tar sudo micro coreutils git tidy oraclejre8 vlc 7zip paint.net gitkraken filezilla sqlitebrowser putty anaconda3 nodejs python go sharpkeys nssm draw.io ssh-copy-id perl terminus plex-player latex speedtest-cli 
+scoop install busybox curl grep nano vim make say tar sudo micro coreutils git tidy oraclejre8 vlc 7zip paint.net gitkraken filezilla sqlitebrowser putty anaconda3 nodejs python go sharpkeys nssm draw.io ssh-copy-id perl terminus plex-player latex speedtest-cli win-acme
 
 # Installing Visual Studio Code Sync Extension and the themes
 code --install-extension shan.code-settings-sync

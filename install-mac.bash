2bfbdcf5b63f5e2e7ddc47482beec858b695b5a3
Jakob Hviid <jakob@Chronos.local>
2020-06-03 16:02:51 +0200

    ..

--- install-mac.bash
+++ install-mac.bash
@@ -2,6 +2,8 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 brew install cask
 brew cask install mactex
 brew cask install dotnet-sdk
@@ -10,3 +12,50 @@ brew cask install bartender
 brew cask install drawio
 brew cask isntall microsoft-teams
 brew cask install discord
+brew cask install omnigraffle
+brew cask install intellij-idea
+brew cask install datagrip
+
+brew install python
+easy_install pip
+brew cask install qlimagesize
+brew cask install qlcolorcode
+brew cask install qlmarkdown
+brew cask install suspicious-package
+brew cask install quicklook-json
+
+brew install midnight-commander
+brew install htop
+brew install mas
+mas install 1333542190
+mas install 462058435
+mas install 153157709
+mas install 449412482
+mas install 82658836
+mas install 25264550
+mas install 29449079
+mas install 62054704
+mas install 24726344
+mas install 37984704
+mas install 055273043
+mas install 289583905
+mas install 41258766
+mas install 25424353
+mas install 274495053
+mas install 62062816
+mas install 339170533
+mas install 295203466
+mas install 23766827
+mas install 176074088
+mas install 09201541
+mas install 57734279
+mas install 84801555
+mas install 85367838
+mas install 90588172
+mas install 480068668
+mas install 08981434
+mas install 409203825
+mas install 967805235
+mas install 409183694
+mas install 1040002810
+mas install 1107421413
